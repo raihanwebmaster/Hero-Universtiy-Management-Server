b63688d88f3787039368bb20caa4060cfc3dd1bc
raihanwebmaster <raihanemon2015@gmail.com>
2023-12-25 17:37:00 +0100

    update

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -2,16 +2,22 @@ import express from 'express';
 import { StudentControllers } from './student.controller';
 import validateRequest from '../../middlware/validateRequest';
 import { StudentValidationSchema } from './student.validation';
+import auth from '../../middlware/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
 router.get('/', StudentControllers.getAllStudents);
-router.get('/:id', StudentControllers.getSingletudent);
+router.get(
+  '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.faculty),
+  StudentControllers.getSingletudent,
+);
 router.patch(
   '/:id',
   validateRequest(StudentValidationSchema.updateStudentValidationSchema),
   StudentControllers.updateStudent,
 );
-router.delete('/:id', StudentControllers.deleteStudent);
+router.delete('/:id', auth(USER_ROLE.admin), StudentControllers.deleteStudent);
 
 export const StudentRoutes = router;
