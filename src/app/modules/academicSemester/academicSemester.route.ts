0f23551053a3f381cd55f055801833e049cb54c7
raihanwebmaster <raihanemon2015@gmail.com>
2024-01-20 18:40:24 +0100

    protect academic semesters get api

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { AcademicSemesterControllers } from './academicSemester.controller';
 import validateRequest from '../../middlware/validateRequest';
 import { academicSemesterValidation } from './academicSemester.validaton';
+import auth from '../../middlware/auth';
+import { USER_ROLE } from '../user/user.constant';
 const router = express.Router();
 
 router.post(
@@ -11,10 +13,7 @@ router.post(
   ),
   AcademicSemesterControllers.createAcademicSemester,
 );
-router.get(
-  '/:id',
-  AcademicSemesterControllers.getSingleAcademicSemester,
-);
+router.get('/:id', AcademicSemesterControllers.getSingleAcademicSemester);
 
 router.patch(
   '/:id',
@@ -24,6 +23,10 @@ router.patch(
   AcademicSemesterControllers.updateAcademicSemester,
 );
 
-router.get('/', AcademicSemesterControllers.getAllAcademicSemesters);
+router.get(
+  '/',
+  auth(USER_ROLE.admin),
+  AcademicSemesterControllers.getAllAcademicSemesters,
+);
 
 export const AcademicSemesterRoutes = router;
